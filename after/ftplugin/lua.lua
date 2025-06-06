b5c80e0f29118d3d5dfe36067269c34b4acd30f0
Miroslav <miroslavsotirov1@gmail.com>
2025-06-06 13:25:51 +0300

    Removed Kickstarter crutches!

---
+++ after/ftplugin/lua.lua
@@ -0,0 +1,4 @@
+vim.opt_local.tabstop = 2 -- Set tab display width to 4
+vim.opt_local.softtabstop = 2 -- Set tab display width to 4
+vim.opt_local.shiftwidth = 2 -- Set auto-indent width to 4
+vim.opt_local.expandtab = true -- Ensure spaces are used instead of tabs
---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,20 @@
+return {
+  'nvimtools/none-ls.nvim',
+  config = function()
+    local null_ls = require 'null-ls'
+    null_ls.setup {
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.goimports,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.markdownlint,
+
+        -- null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.diagnostics.hadolint,
+        null_ls.builtins.diagnostics.markdownlint,
+      },
+
+      vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, {}),
+    }
+  end,
+}
