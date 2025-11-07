44582db0e61d27a49bb6648bd816ad4583ec71fa
BatMichoo <miroslavsotirov1@gmail.com>
2025-11-07 15:55:10 +0200

    Easy-Dotnet

--- lua/plugins/roslyn-extensions.lua
+++ lua/plugins/roslyn-extensions.lua
@@ -1,9 +1,9 @@
 return {
-  -- 'seblyng/roslyn.nvim',
-  -- -- The 'ft' (filetype) option tells lazy.nvim to load this plugin
-  -- -- only when one of these filetypes is detected.
-  -- ft = { 'cs', 'cshtml', 'csproj', 'sln' },
-  -- opts = {
-  --   silent = true,
-  -- },
+  'seblyng/roslyn.nvim',
+  -- The 'ft' (filetype) option tells lazy.nvim to load this plugin
+  -- only when one of these filetypes is detected.
+  ft = { 'cs', 'cshtml', 'csproj', 'sln' },
+  opts = {
+    silent = true,
+  },
 }
