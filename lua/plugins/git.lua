96799dfcf6db81f0ffce7a1cf66d434a61f5430e
BatMicho <miroslavsotirov1@gmail.com>
2026-06-08 17:02:41 +0300

    feat: configure lazygit for dotfiles bare repo when in HOME

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -15,6 +15,12 @@ return {
   },
   config = function()
     require('telescope').load_extension 'lazygit'
+
+    -- Check if Neovim was opened in the home directory, and configure lazygit for dotfiles bare repo
+    if vim.fn.getcwd() == vim.fn.expand("$HOME") then
+      vim.fn.setenv("GIT_DIR", vim.fn.expand("$HOME/.dotfiles"))
+      vim.fn.setenv("GIT_WORK_TREE", vim.fn.expand("$HOME"))
+    end
   end,
   keys = {
     { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
